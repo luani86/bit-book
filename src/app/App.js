7f23363fff24c9e690664331d6c108e1c1d36aec
BIT Student <classroom@bgit.rs>
2018-03-27 17:38:30 +0200

    feed list page on progress

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import FeedListPage from './pages/FeedListPage';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <FeedListPage/>
+      </div>
+    );
+  }
+}
+
+export default App;
