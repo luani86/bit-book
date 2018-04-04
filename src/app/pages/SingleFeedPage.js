0fbcbf15c0c06502cabf1172e2d0260cbae3c202
BIT Student <classroom@bgit.rs>
2018-04-04 10:31:35 +0200

    f4 done

--- src/app/pages/SingleFeedPage.js
+++ src/app/pages/SingleFeedPage.js
@@ -18,7 +18,6 @@ class SingleFeedPage extends React.Component {
         }
     }
 
-
     componentDidMount() {
         this.getPostById();
         this.getComments();
@@ -50,7 +49,6 @@ class SingleFeedPage extends React.Component {
                     comments
                 });
             });
-
     }
 
     renderPost() {
@@ -64,153 +62,54 @@ class SingleFeedPage extends React.Component {
     }
 
     handlePostComments = (event) => {
-        this.setState({commentText: event.target.value})
+        this.setState({ commentText: event.target.value })
     }
-
-   
 
     submitComment = () => {
-        
         postService.postComments(this.state.commentText, this.props.match.params.id)
-        .then((response => {
-            this.getComments()
-            this.clearInput()
-        }))
-        
-    }
-    clearInput = () => {
-        this.setState({commentText: ""})
+            .then((response => {
+                this.getComments()
+                this.clearInput()
+            }))
+
     }
 
+    renderComments = () => {
+        if (this.state.comments.length !== 0) {
+            this.state.comments.map(comment => {
+                return <SingleComment commentInfo={comment} />
+            });
+        } else {
+            return (
+                <div>
+                    <h3>There are no comments for this post.</h3>
+                </div>
+            )
+        }
+    }
+
+    clearInput = () => {
+        this.setState({ commentText: "" })
+    }
 
     render() {
-return (
-    <div>
-        {this.renderPost()}
-        <div className="col s12 m7">
-    
-    
-    
-    {this.state.comments.map(comment => {
-        return <SingleComment commentInfo={comment} />
-    })
-    }
-</div>
-<div className="row">
-          <div className="input-field col s10 ">
-            <textarea id="textarea1" className="materialize-textarea" onChange={this.handlePostComments} value={this.state.commentText}></textarea>
-            <label htmlFor="textarea1">Post comment
-          
-            </label>
-              <button className="col s2 btn waves-effect waves-light" type="submit" name="action" onClick={this.submitComment}>Submit
-    <i className="material-icons right">send</i>
-  </button>
-          </div>
-          
-        </div>
-    
-    </div>
-);
-       
-
-
-                            
-
-
-        // console.log(this.state.post)
-        // if(this.props.match.params.type === "TextPosts") {
-        //     return (
-        //         <div className="container">
-        //             <div className="row">
-        //                 <div className="col s12 m6 offset-m3">
-        //                     <div className="card green lighten-1">
-        //                         <div className="card-content white-text">
-                                    
-        //                             <p>{this.state.post.text}</p>
-        //                         </div>
-        //                         <div className="card-action">
-    
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-    
-        //             <div className="col s12 m7">
-    
-    
-    
-        //                 {this.state.comments.map(comment => {
-        //                     return <SingleComment commentInfo={comment} />
-        //                 })
-        //                 }
-    
-    
-        //             </div>
-        //         </div>
-        //     )
-        // } else if (this.props.match.params.type === "ImagePosts") {
-        //     return (
-        //         <div className="container">
-        //             <div className="row">
-        //                 <div className="col s12 m6 offset-m3">
-        //                     <div className="card green lighten-1">
-        //                         <div className="card-content white-text">
-                                    
-        //                             <img src={this.state.post.imageUrl}></img>
-        //                         </div>
-        //                         <div className="card-action">
-    
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-    
-        //             <div className="col s12 m7">
-    
-    
-    
-        //                 {this.state.comments.map(comment => {
-        //                     return <SingleComment commentInfo={comment} />
-        //                 })
-        //                 }
-    
-    
-        //             </div>
-        //         </div>
-        //     )
-        // }
-        // else {
-        //     return (
-        //         <div className="container">
-        //             <div className="row">
-        //                 <div className="col s12 m6 offset-m3">
-        //                     <div className="card green lighten-1">
-        //                         <div className="card-content white-text">
-                                    
-        //                             <p>{this.state.post.video}</p>
-        //                         </div>
-        //                         <div className="card-action">
-    
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-    
-        //             <div className="col s12 m7">
-    
-    
-    
-        //                 {this.state.comments.map(comment => {
-        //                     return <SingleComment commentInfo={comment} />
-        //                 })
-        //                 }
-    
-    
-        //             </div>
-        //         </div>
-        //     )
-        // }
-        
+        console.log(this.state.comments.length)
+        return (
+            <div>
+                {this.renderPost()}
+                <div className="col s12 m7">
+                    {this.renderComments()}
+                </div>
+                <div className="row">
+                    <div className="input-field col s10 ">
+                        <textarea id="textarea1" className="materialize-textarea" onChange={this.handlePostComments} value={this.state.commentText}></textarea>
+                        <label htmlFor="textarea1">Post comment</label>
+                        <button className="col s2 btn waves-effect waves-light" type="submit" name="action" onClick={this.submitComment}>Submit<i className="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
