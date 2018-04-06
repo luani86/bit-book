fd555d2f2fad946f28cc01c6520f461d10a4bee3
BIT Student <classroom@bgit.rs>
2018-04-06 15:35:26 +0200

    log out done(?)

--- src/app/partials/Header.js
+++ src/app/partials/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-
+import { userService } from '../../services/userService';
 
 const Header = () => {
     return (
@@ -10,10 +10,10 @@ const Header = () => {
                     <span id="title">BitBook</span>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
 
-                        <li><Link to="LoginPage" className="nav-item nav-link active">Home</Link></li>
                         <li><Link to="/FeedListPage" className="nav-item nav-link active">Feed</Link></li>
                         <li><Link to="/PeopleList" className="nav-item nav-link active">People</Link></li>
                         <li><Link to='/MyProfilePage' className='header_text'>Profile</Link></li>
+                        <li onClick={userService.logout}>Logout</li>
 
                     </ul>
                 </div>
